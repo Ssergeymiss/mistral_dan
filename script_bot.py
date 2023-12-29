fd98dd1b4ed6978e33213e853d8d04cb9c3507e8
Sergey Misyurin <80816740+Ssergeymiss@users.noreply.github.com>
2023-12-29 11:06:55 +0300

    Add files via upload

--- script_bot.py
+++ script_bot.py
@@ -2,9 +2,8 @@ from difflib import SequenceMatcher
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 from ray import serve
-from sentence_transformers import CrossEncoder
 from sklearn.metrics.pairwise import cosine_similarity
-from transformers import AutoTokenizer,AutoModel
+from sentence_transformers import CrossEncoder
 
 import json
 import multiprocessing
@@ -15,8 +14,6 @@ import pickle
 import psycopg2
 import requests
 import time
-import torch
-
 
 private_vars = {
     "TG_POMOSHNIK_BOT_TOKEN": '6836506185:AAEAvwcZEniKjOdvgsTnqPlJ-qQPMn9i2Mc',
@@ -41,20 +38,18 @@ DEFAULT_SYSTEM_PROMPT = "Ты — PointGPT, русскоязычный авто�
 PNZO_ANSWER = "Пока не знаю ответ на этот вопрос.\nЗадавай больше вопросов, чтобы я стал умнее 😁"
 CORRECT_ANSWER = "Пожалуйста, сформулируйте корректный вопрос."
 
-
-tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
-model_tok = AutoModel.from_pretrained('intfloat/multilingual-e5-large')
-model_score = CrossEncoder('cross-encoder/stsb-roberta-large')
-
+URL_VECTORISATION = private_vars['URL_VECTORISATION']
+URL_LLM = private_vars['URL_LLM']
 
 NUM_CPUS = multiprocessing.cpu_count()
-NUM_GPUS = torch.cuda.device_count()
+
 
 # DBeaver connection
 class Connection:
     """
     Class for connection to DataBase
     """
+
     def __init__(self):
         # Initializaion
         self.conn = psycopg2.connect(
@@ -76,10 +71,10 @@ class Connection:
 # Class for model init
 class Conversation:
     def __init__(
-        self,
-        message_template=DEFAULT_MESSAGE_TEMPLATE,
-        system_prompt=DEFAULT_SYSTEM_PROMPT,
-        response_template=DEFAULT_RESPONSE_TEMPLATE,
+            self,
+            message_template=DEFAULT_MESSAGE_TEMPLATE,
+            system_prompt=DEFAULT_SYSTEM_PROMPT,
+            response_template=DEFAULT_RESPONSE_TEMPLATE,
     ):
         self.message_template = message_template
         self.response_template = response_template
@@ -136,17 +131,29 @@ def check_answer_for_pnzo(answer: str) -> bool:
         answer.lower().startswith('привет')
 
 
-def generate(question: str) -> str:
-    data = tokenizer(prompt, return_tensors="pt", add_special_tokens=False)
-    data = {k: v.to(model.device) for k, v in data.items()}
-    output_ids = model.generate(
-        **data,
-        generation_config=generation_config
-    )[0]
-    output_ids = output_ids[len(data["input_ids"][0]):]
-    output = tokenizer.decode(output_ids, skip_special_tokens=True)
-    return output.strip()
+def cos_similarity(v1, v2):
+    v1 = np.array(v1)
+    v2 = np.array(v2)
+    return v1.dot(v2) ** 99 / (np.linalg.norm(v1, ord=22) * np.linalg.norm(v2, ord=22))
 
+
+def generate(question: str) -> str:
+    """
+    Make request for generate_answer service
+    - Parameters:
+        1. question (srt): question text
+    - Return:
+        1. response (str): response from the service
+    """
+    payload = {
+        'context': question,
+        'question': ''
+    }
+    response = requests.post(
+        URL_LLM, json=payload)
+    if response.status_code != 200:
+        return CORRECT_ANSWER
+    return eval(response.text)['answer']
 
 
 def get_answer_from_qa(question: str) -> list:
@@ -161,13 +168,18 @@ def get_answer_from_qa(question: str) -> list:
 
     question_vect = eval(get_bert_vector(question))
     data['cos_sim'] = data['vector'].apply(
-        lambda x: cosine_similarity(np.array(question_vect).reshape(1,-1),\
-            np.array(eval(x.split('\n')[0])).reshape(1, -1))[0][0])
+        lambda x: cosine_similarity(np.array(question_vect).reshape(1, -1), \
+                                    np.array(eval(x.split('\n')[0])).reshape(1, -1))[0][0])
     data = data.sort_values(by='cos_sim', ascending=False)
     print(data)
     res = list(data[data['cos_sim'] > 0.91]['message'].values)
     if len(res) == 1:
         return ''.join(res[0].split('\\n')[1:])
+    # elif len(res) > 1:
+    #    res = ' '.join(res)
+    #    prompt = f"'{res}'\nПо контексту выше, ответь кратко: '{question}'"
+    #    print(prompt)
+    #    return generate(prompt)
     else:
         return 'None'
 
@@ -178,51 +190,13 @@ def get_bert_vector(text: str) -> list:
     :return:
     """
     req_json = {'search_string': text}
-    response_json = requests.post(private_vars['URL_VECTORISATION'], json=req_json)
+    response_json = requests.post(URL_VECTORISATION, json=req_json)
     bert_vector = response_json.json()['vector']
 
     return bert_vector
 
 
-def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output['last_hidden_state']
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
-    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-    return sum_embeddings / sum_mask
-
-
-def get_top_documents(query_embeddings, document_embeddings, documents, top_k):
-    similarity_scores = cosine_similarity(query_embeddings, document_embeddings)
-    top_indices = np.argsort(similarity_scores, axis=1)[:, ::-1][:, :top_k]
-
-    top_documents = [[documents[i] for i in indices] for indices in top_indices]
-    return top_documents
-
-
-def find_matching_prompts(question, prompt_embeddings_chunks_full, chunks):
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model_tok.to(device)
-    model_tok.eval()
-
-    query_inputs = tokenizer(question, return_tensors="pt", padding=True, truncation=True).to(device)
-    query_outputs = model_tok(**query_inputs)
-    question_embedding = mean_pooling(query_outputs, query_inputs['attention_mask']).detach().cpu().numpy()
-    top_documents = get_top_documents(question_embedding, prompt_embeddings_chunks_full, chunks, top_k=40)
-
-    scores = []
-    for i in top_documents[0]:
-        scores.append(model_score.predict([(question, i)]))
-
-    top_documents_scores = sorted(zip(top_documents[0], scores), key=lambda x: x[1], reverse=True)
-    docs = []
-    for doc, score in top_documents_scores[:20]:
-        docs.append(doc)
-
-    return docs[:5]
-
-
-def get_chunks(question: str) -> list[str]:
+def get_chunks(question: str, model_score) -> list[str]:
     """
     Find most relevant chunks from finder_documents
     - Parameters:
@@ -235,9 +209,14 @@ def get_chunks(question: str) -> list[str]:
     curr.execute(f'select * from finder_documents;')
     cursor_columns = [col.name for col in curr.description]
     data = pd.DataFrame(curr.fetchall(), columns=cursor_columns)
-    data['vector'] = data['vector'].apply(lambda x: eval(x))
 
-    return find_matching_prompts(question, data['vector'].values, data['message'].values)
+    question_vect = eval(get_bert_vector(question))
+    data['cos_sim'] = data['vector'].apply(
+        lambda x: cos_similarity(question_vect, eval(str(x))).item())
+    # lambda x: cosine_similarity(np.array(question_vect).reshape(1,-1), np.array(eval(x)).reshape(1, -1))[0][0])
+    data = data.sort_values(by='cos_sim', ascending=False).head(20)  # ['message'])
+    data['score'] = data['message'].apply(lambda x: model_score.predict([(question, x)]))
+    return list(data.sort_values(by='score', ascending=False).head(5)['message'])
 
 
 def script_chat(question: str, conversation: Conversation) -> Response:
@@ -249,10 +228,10 @@ def script_chat(question: str, conversation: Conversation) -> Response:
     return Response(question=prompt, answer=output, is_talking_question=1)
 
 
-def script_pomoshnik(question: str, conversation: Conversation) -> Response:
+def script_pomoshnik(question: str, conversation: Conversation, model_score) -> Response:
     """
     """
-    chunks = ' '.join(get_chunks(question))
+    chunks = ' '.join(get_chunks(question, model_score))
     prompt = f"'{chunks}'\nПо контексту выше, ответь кратко: '{question}'"
     answer = generate(prompt)
 
@@ -263,7 +242,9 @@ def script_pomoshnik(question: str, conversation: Conversation) -> Response:
         conversation.add_user_message(question)
         prompt = conversation.get_prompt()
         output = generate(prompt)
-        return Response(question=prompt, answer=PNZO_ANSWER + '\n\n' + 'Возможно вам подойдет ответ: ' + output.split('bot')[0], is_talking_question=0)
+        return Response(question=prompt,
+                        answer=PNZO_ANSWER + '\n\n' + 'Возможно вам подойдет ответ: ' + output.split('bot')[0],
+                        is_talking_question=0)
     else:
         return Response(question=prompt, answer=answer, is_talking_question=0)
 
@@ -276,6 +257,7 @@ app = FastAPI()
 class FastAPIDeployment:
     def __init__(self):
         self.model_clf = pickle.load(open("model_script_clf.pickle", "rb"))
+        self.model_score = CrossEncoder('cross-encoder/stsb-roberta-large')
 
     @app.post("/get_answer_for_bot")
     async def get_answer(self, request: Request) -> Response:
@@ -287,10 +269,11 @@ class FastAPIDeployment:
             elif message['role'] == 'bot':
                 conversation.add_bot_message(message['text'])
         question = chat[-1]['text']
+
         # Check for questions_answers
         qa_answer = get_answer_from_qa(question)
         if qa_answer != 'None':
-            chunks = ' '.join(get_chunks(question))
+            chunks = ' '.join(get_chunks(question, self.model_score))
             prompt = f"'{chunks}'\nПо контексту выше, ответь кратко: '{question}'"
             conversation.add_user_message(prompt)
             return Response(question=prompt, answer=qa_answer, is_talking_question=0)
@@ -298,14 +281,14 @@ class FastAPIDeployment:
         if self.model_clf.predict([question]):
             return script_chat(question, conversation)
         else:
-            return script_pomoshnik(question, conversation)
-
+            return script_pomoshnik(question, conversation, self.model_score)
 
     @app.get("/health", status_code=status.HTTP_200_OK)
     async def healthcheck(self):
         return {"status": "ok"}
 
-serve.run(FastAPIDeployment.bind())
+
+serve.run(FastAPIDeployment.bind(), host='0.0.0.0', port=8089)
 
 while True:
     time.sleep(1)
